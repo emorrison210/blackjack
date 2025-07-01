238ef1dc80f0fac2d63e56f3096bdd61c56fcb23
Evan Morrison <emorrison210@gmail.com>
2025-06-30 22:35:26 -0400

    just needs bug fixing and testing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 cards = {
     "1": 1,
@@ -71,9 +72,10 @@ def main():
         p2_points = 0
         p1_cards = []
         p2_cards = []
-        bet = int(input("Place a bet: "))
-        print("Drawing 2 cards to player and host...")
-        print("--- Your Cards ---")
+        bet = int(input("\nPlace a bet: "))
+        print("\nDrawing 2 cards to player and host...\n")
+        time.sleep(2) # time until next line is executed
+        print("--- Your Cards ---\n")
             
         # cards get dealt
         p1_cards.extend(deal(2))
@@ -82,7 +84,7 @@ def main():
         # cards points get added up
         p1_points = calculate_points(p1_cards)
         p2_points = calculate_points(p2_cards)
-        print(f"{p1_cards} = {p1_points} points")
+        print(f"{p1_cards} = {p1_points} points\n")
         
         # determine if someone got 21 off first two cards         
         if determine_winner(p1_points, p2_points) == "Player 1":
@@ -97,20 +99,66 @@ def main():
             print("Tie Game")
             continue
         
+        # just shows the dealers face up card that got dealt second
+        print("--- Dealer's Cards ---\n")
+        print(f"Facedown and {p2_cards[1]}\n")
+        
+        # if no one won got 21 of the first 2 cards - draw another yes or no?
         while True:
-            deal_another = input("Do you want to another card? Y/N ")
+            deal_another = input("Hit or Stand? Y/N \n")
+            time.sleep(2)
             if deal_another == "y":
                 p1_cards.extend(deal(1))
                 p1_points = calculate_points(p1_cards)
-                print(f"{p1_cards} = {p1_points} points")
+                print(f"\n{p1_cards} = {p1_points} points\n")
+                if p1_points > 21:
+                    money_made -= bet
+                    print(f"You went over 21 and lost. You lost ${bet}")
+                    break
+                else:
+                    continue
             if deal_another != "y" and deal_another != "n":
                 continue
-            else:
-                break
+            
+            # dealer decides whether to take a card or not
+            while p2_points <= 14:
+                p2_cards.extend(deal(1))
+                p2_points = calculate_points(p2_cards)
+                print(f"Dealer gets handed a {p2_cards[2]}\n")
+            time.sleep(2)
+            print("Dealer decides to not draw another card and flips his facedown card\n")
+            time.sleep(1.3)
+            print(f"Dealers cards: {p2_cards} = {p2_points}\n")
+            break
+        
+        if determine_winner(p1_points, p2_points) == "Player 1":
+            money_made += bet
+            print(f"Player 1 Wins! You just won ${bet}\n{money_made}")
+            continue
+        elif determine_winner(p1_points, p2_points) == "Player 2":
+            money_made -= bet
+            print(f"Player 2 Wins! You just lost ${bet}\n{money_made}")
+            continue
+        elif determine_winner(p1_points, p2_points) == "Tie":
+            print("Tie Game")
+            continue
+        elif p1_points > p2_points:
+            money_made += bet
+            print(f"Player 1 Wins! You just won ${bet}. Earnings so far: {money_made}\n")
+            continue
+        elif p2_points > p1_points:
+            money_made -= bet
+            print(f"Player 2 Wins! You just lost ${bet}. Earnings so far: {money_made}\n")
+            continue
+        else:
+            print("Tie Game\n")
+            print(f"Earnings so far: {money_made}\n")
+            continue
+        
             
         
-        print("hi")
-        print(f"You have made ${money_made}.")
+        #print("test")
+        #print(f"You have made ${money_made}.")
                 
             
             
